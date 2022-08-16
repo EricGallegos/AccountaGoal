7f4db132743ad1aeb464f9ddfa805964d1a2075a
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-08-16 16:15:01 -0600

    fixed redirect after authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   ensureGuest: function(req, res, next){
     if (req.isAuthenticated()){
-      res.redirect('/content')
+      res.redirect('/dashboard')
     }
     else{
       return next();
