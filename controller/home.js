2d0ca27d75e44884f0e1a57cd263da3240a83d29
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-08-17 18:42:15 -0600

    Goal History basic functionality

--- controller/home.js
+++ controller/home.js
@@ -13,6 +13,7 @@ module.exports = {
   // @route    GET /dashboard
   getDashboard: async (req, res) => {
     try {
+      //Get repeating goals + todays goals and combine them for viewing
       const repeatingGoals = await Goals.find({
                                       user: req.user.id,
                                       repeating: true,
@@ -25,8 +26,14 @@ module.exports = {
                                     }).lean();
       todaysGoals = repeatingGoals.concat(todaysGoals);
 
-      let upcoming = new Array;
+      let chartData = generateChart(await Goals.find({user: req.user.id,}).lean(), await Goals.find({
+        user: req.user.id,
+        completedOn: {$ne: null},
+        $expr: {$lt:["completeOn", "dueDate"]}
+      }).lean())
 
+      //Get goals for user for next 4 days
+      let upcoming = new Array;
       for( let i = 0; i < 4; i++){
         upcoming.push(await Goals.find({
                                         user: req.user.id,
@@ -35,6 +42,8 @@ module.exports = {
                                         repeating: false,
                                       }).lean() )
       }
+
+      //Attach the goals to an object that also has the name of the day
       let futureGoals = new Array;
       for( let i = 0; i < 4; i++ ){
         futureGoals.push({name: moment(new Date()).add(i+1, 'days').format('dddd'),
@@ -45,6 +54,7 @@ module.exports = {
       res.render('dashboard', {
         name: req.user.firstName,
         todaysGoals,
+        chartData,
         futureGoals,
       });
     } catch (e) {
@@ -52,4 +62,39 @@ module.exports = {
       res.render('error/500');
     }
   },
+}
+
+function generateChart(all, completed){
+  let totalNum;
+  let completedNum;
+  let dailyValues = [];
+
+  for (let i = 0; i < 140; i++){
+    totalNum = 0;
+    completedNum = 0;
+    let start = moment(new Date).startOf('day').add(-i, 'days').toDate();
+
+    all.forEach(goal =>{
+      if( goal.startDate.getTime() == start.getTime()){
+        totalNum++;
+        if( goal.completedOn ){
+          completedNum++;
+        }
+      }
+    })
+    if(totalNum == 0 || completedNum == 0) {
+      dailyValues.push(0);
+    }else{
+      dailyValues.push(completedNum/totalNum)
+    }
+  }
+  dailyValues = dailyValues.map(val =>{
+    if( val == 0) return 0;
+    if( val > 0 && val <= .2) return .2;
+    if( val > .2 && val <= .4) return .4;
+    if( val > .4 && val <= .6) return .6;
+    if( val > .6 && val <= .8) return .8;
+    if( val > .8 && val <= 1) return 1;
+  })
+  return dailyValues;
 }
