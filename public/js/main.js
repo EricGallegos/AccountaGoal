2e311699a5be2283f60aef7a1701ee4d1df28f5b
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-08-19 17:26:12 -0600

    testing carousel options

--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,19 @@ document.querySelector('#carouselForward').addEventListener('click', (event) => 
     $slide.scrollIntoView();
   }
 });
+
+// goals = document.querySelectorAll('.simpleMain');
+// goals.forEach( goal =>{
+//   goal.addEventListener('click', (event) => {
+//     const $slide = document.querySelector(event.target.getAttribute('href'));
+//     if (!$slide) return;
+//
+//     if ($slide.scrollIntoViewIfNeeded) {
+//       event.preventDefault();
+//       $slide.scrollIntoViewIfNeeded();
+//     } else if ($slide.scrollIntoView) {
+//       event.preventDefault();
+//       $slide.scrollIntoView();
+//     }
+//   });
+// })
