62f4619170e4fadeff703e408c66164e7c3996f7
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-08-23 19:08:42 -0600

    date format change in upcoming

--- controller/home.js
+++ controller/home.js
@@ -101,7 +101,7 @@ module.exports = {
       //Attach the goals to an object that also has the name of the day
       let futureGoals = new Array;
       for( let i = 0; i < 6; i++ ){
-        futureGoals.push({name: moment(now).add(i+1, 'days').format('dddd'),
+        futureGoals.push({name: moment(now).add(i+1, 'days').format('dddd, MMM Do'),
                           goals: upcoming.shift(),
                         })
       }
